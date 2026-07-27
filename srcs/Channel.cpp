697563351b7a3cee09200aa955877ead52d50387
Oussama Ait H'mad <oussamaaithmed1@gmail.com>
2026-07-27 19:28:28 +0100

    add channel manager

---
+++ srcs/Channel.cpp
@@ -0,0 +1,37 @@
+#include "../Includes/Channel.hpp"
+
+Channel::Channel() {}
+
+Channel::Channel(const std::string &name) : _name(name) {}
+
+Channel::~Channel() {}
+
+const std::string &Channel::getName() const
+{
+    return _name;
+}
+
+void Channel::addClient(Client *client)
+{
+    _members.insert(client);
+}
+
+void Channel::removeClient(Client *client)
+{
+    _members.erase(client);
+}
+
+bool Channel::isMember(Client *client) const
+{
+    return _members.find(client) != _members.end();
+}
+
+bool Channel::isEmpty() const
+{
+    return _members.empty();
+}
+
+size_t Channel::getMemberCount() const
+{
+    return _members.size();
+}
