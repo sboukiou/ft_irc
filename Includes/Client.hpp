b86b2afd9cb6867676ec77ef0b0d8017741d0605
hmouis <houdayfamouis@gmail.com>
2026-07-27 19:32:12 +0100

    add the fd of the client and the buffer to save the data that the client sent and the setters, getters for this attibute

--- Includes/Client.hpp
+++ Includes/Client.hpp
@@ -1,13 +1,25 @@
 #ifndef Client_hpp
 #define Client_hpp
 
+#include <iostream>
+
 class Client{
-    
+    private:
+        int _fd;
+        std::string buffer;
+        bool is_complete;
+
     public:
         Client();
+        Client(int fd);
         Client(const Client& other);
         Client& operator=(const Client& other);
         ~Client();
+
+        void setIsCoplete(bool status);
+        bool getIsComplete() const;
+        void setBuffer(std::string buf);
+        std::string getBuffer() const;
 };
 
 
