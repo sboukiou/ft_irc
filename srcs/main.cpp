da51a63b8b8194e46165d52e3253d3fecadbfe67
sboukiou <said.perso@outlook.com>
2026-07-29 19:41:02 +0000

    ADD: Declaring and running a server in the main function

--- srcs/main.cpp
+++ srcs/main.cpp
@@ -1,10 +1,18 @@
 #include "../include/Server.hpp"
 #include "../include/Client.hpp"
-#include <cstdio>
+#include <cstdlib>
 
 
 int main(int ac, char **av) {
-	Server server;
+	int	port;
+	std::string password;
+
+	if (ac != 3) {
+		std::cerr << "Invalid number of arguments!\n";
+	}
+	port = atoi(av[1]);
+	password = std::string(av[2]);
+	Server server(port, password);
 
 	server.run();
 	return (0);
