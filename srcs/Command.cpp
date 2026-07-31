235dd2b625f9e948e6e7c4f1ff74bc0a68136765
sboukiou <said.perso@outlook.com>
2026-07-31 10:13:09 +0000

    Add: Implementation of the Command class

---
+++ srcs/Command.cpp
@@ -0,0 +1,39 @@
+#include "../include/Command.hpp"
+#include <stdexcept>
+
+Command::Command(): _name(""), _args(0) {}
+Command::Command(std::string line) {
+	size_t start = 0;
+	size_t end = line.find(' ');
+	if (end == std::string::npos)
+		throw(std::runtime_error("Invalid command"));
+	_name = line.substr(start, end - start);
+	start = end + 1;
+	while (start < line.size() && end != std::string::npos) {
+		end = line.find(' ');
+		_args.push_back(line.substr(start, end -  start));
+		start = end + 1;
+	}
+}
+Command::Command(const Command &other) {
+	if (this != &other) {
+		_name = other.getName();
+		_args = other.getArgs();
+	}
+}
+Command &Command::operator=(const Command &other) {
+	if (this != &other) {
+		_name = other.getName();
+		_args = other.getArgs();
+	}
+	return (*this);
+}
+Command::~Command() {}
+
+const std::string &Command::getName(void) const {
+	return (_name);
+}
+
+const std::vector<std::string> &Command::getArgs(void) const {
+	return (_args);
+}
