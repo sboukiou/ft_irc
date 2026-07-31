b80f6586988a5019ba159e48233eb85a96292004
sboukiou <said.perso@outlook.com>
2026-07-31 10:12:52 +0000

    add: Definition for the command class

---
+++ include/Command.hpp
@@ -0,0 +1,41 @@
+#ifndef COMMAND_HPP
+# define COMMAND_HPP
+
+#include <string>
+#include <vector>
+enum requestType {
+	PASS,
+	NICK,
+	USER,
+	PING,
+	PONG,
+	QUIT,
+	JOIN,
+	PART,
+	PRIVMSG,
+	NOTICE,
+	MODE,
+	TOPIC,
+	KICK,
+	INVITE,
+	NAMES,
+	LIST,
+	WHO,
+	WHOIS,
+};
+
+class Command {
+	private:
+		std::string _name;
+		std::vector<std::string> _args;
+	public:
+		Command();
+		Command(std::string line);
+		Command(const Command &other);
+		Command &operator=(const Command &other);
+		~Command();
+		const std::string &getName(void) const;
+		const std::vector<std::string> &getArgs(void) const;
+};
+
+#endif /* COMMAND_HPP */
