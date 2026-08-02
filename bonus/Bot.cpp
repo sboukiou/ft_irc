68d70f57dc21e307b17c3613b8d9d83851dbb72b
Oussama Ait H'mad <oussamaaithmed1@gmail.com>
2026-08-02 11:15:40 +0100

    implement !ping, !time, !help

--- bonus/Bot.cpp
+++ bonus/Bot.cpp
@@ -2,15 +2,21 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 #include <arpa/inet.h>
-
+#include <bits/stdc++.h>
+#include "../include/macros.hpp"
+#include <ctime>
 
 Bot::Bot() {}
 
-Bot::Bot(int port, std::string host, std::string pass, std::string nick, std::string user, std::string name) : 
-_port(port), _host(host), _pass(pass), _nick(nick), _user(user), _name(name){}
+Bot::Bot(int port, std::string host, std::string pass,
+    std::string nick, std::string user, std::string name) : 
+_port(port), _host(host), _pass(pass), 
+_nick(nick), _user(user), _name(name){}
 
 Bot::Bot(const Bot &other) :
-_port(other._port), _host(other._host), _pass(other._pass), _nick(other._nick) {}
+_port(other._port), _host(other._host), _pass(other._pass), 
+_nick(other._nick), _user(other._user), _name(other._name){}
+
 
 Bot::~Bot() {}
 
@@ -19,16 +25,50 @@ bool Bot::connectServer()
     _socketFd = socket(AF_INET, SOCK_STREAM, 0);
     if (_socketFd < 0)
         throw std::runtime_error("Error: Socket failed");
-    sockaddr_in botInfo = {};
+    sockaddr_in botInfo;
+    memset(&botInfo, 0, sizeof(botInfo));
     botInfo.sin_family = AF_INET;
     botInfo.sin_port = htons(_port);
     if (inet_pton(AF_INET, _host.c_str(), &botInfo.sin_addr) <= 0)
-        throw std::runtime_error("Invalid address");
+        throw std::runtime_error("Error: Invalid address");
 
     if (connect(_socketFd, (sockaddr *)&botInfo, sizeof(botInfo)) < 0)
         throw std::runtime_error("Error: connect failed");
 
     return true;
+}
+
+bool Bot::extractLine(std::string& line)
+{
+    size_t pos = _inputBuffer.find('\n');
+    if (pos == std::string::npos)
+        return false;
+    line = _inputBuffer.substr(0, pos);
+    if (!line.empty() && line[line.size() - 1] == '\r')
+        line.erase(line.size() - 1);
+    _inputBuffer.erase(0, pos + 1);
+    return true;
+}
+
+void Bot::receive()
+{
+    char buffer[1024];
+    ssize_t bytes = recv(_socketFd, buffer, sizeof(buffer), 0);
+    if (bytes < 0)
+        throw std::runtime_error("Error: recv failed");
+    else if (bytes == 0)
+    {
+        std::cout << "Server disconnected\n";
+        return ;
+    }
+    _inputBuffer.append(buffer, bytes);
+    std::string line;
+    while (extractLine(line))
+    {
+        std::cout << "Received: " << line << std::endl;
+        handlePing(line);
+        handlePrivmsg(line);
+    }
 }
 
 void Bot::sendRaw(const std::string &msg)
@@ -42,4 +82,53 @@ void Bot::authenticate()
     sendRaw("PASS " + _pass + "\r\n");
     sendRaw("NICK " + _nick + "\r\n");
     sendRaw("USER " + _user + " 0 * :" + _name + "\r\n");
+}
+
+
+void Bot::handlePing(const std::string &msg)
+{
+    size_t pos = msg.find("PING");
+    if (pos == std::string::npos)
+        return ;
+    if (pos != 0)
+        return ;
+    sendRaw("PONG" + msg.substr(4) + "\r\n");
+}
+
+void Bot::execCmd(const std::string &target, const std::string &cmd)
+{
+    if (cmd == "!ping")
+        sendRaw("PRIVMSG " + target + " :pong\r\n");
+    else if (cmd == "!help")
+        sendRaw("PRIVMSG " + target + " :Available commands: !help, !ping, !time, !dice\r\n");
+    else if (cmd == "!time")
+    {
+        std::time_t now = std::time(0);
+        sendRaw("PRIVMSG " + target + " :" + std::ctime(&now) + "\r");
+    }
+
+}
+
+void Bot::handlePrivmsg(const std::string &msg)
+{
+    size_t pos = msg.find("PRIVMSG");
+    if (pos == std::string::npos)
+        return ;
+
+    size_t start = msg.find(":");
+    size_t end = msg.find("!");
+    if (start == std::string::npos || end == std::string::npos)
+        return;
+
+    std::string sender = msg.substr(start + 1, end - start -1);
+    size_t secondColon = msg.rfind(":");
+    std::string messageCmd = msg.substr(secondColon + 1);
+
+    size_t chanStart = pos + 8;
+    size_t chanEnd = msg.find(' ', chanStart);
+    std::string target = msg.substr(chanStart, chanEnd - chanStart);
+    if (target[0] != '#')
+        target = sender;
+
+    execCmd(target, messageCmd);
 }
